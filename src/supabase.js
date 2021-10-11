cf1b677efc81e4f4e6cae80527559511356bbad4
saidheerajv <saidheerajv@gmail.com>
2021-10-11 11:30:34 +0530

    Fixing deployment bug

--- src/supabase.js
+++ src/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 
 const options = {}
-export const supabase = createClient("", "", options);
+export const supabase = createClient("https://rhsugtnhvfojipiyrizv.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTg3MjU0NCwiZXhwIjoxOTQ1NDQ4NTQ0fQ.dCZFzVF9B4CUIkXNPsdXHKDGr9YoT1EOXlxG7yyQaJg", options);
 
