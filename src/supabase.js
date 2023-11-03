832f514679cc67e001eaf4748633af73d3e0ad09
saidheerajv <saidheerajv@gmail.com>
2023-11-03 19:04:43 +0530

    Update - Reconfiguring supabase db

--- src/supabase.js
+++ src/supabase.js
@@ -2,5 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 
 const options = {}
-export const supabase = createClient("https://rhsugtnhvfojipiyrizv.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTg3MjU0NCwiZXhwIjoxOTQ1NDQ4NTQ0fQ.dCZFzVF9B4CUIkXNPsdXHKDGr9YoT1EOXlxG7yyQaJg", options);
+export const supabase = createClient("https://rhsugtnhvfojipiyrizv.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52ZG12dXN2a2h1ZHFsbmdtcmZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkwMTY0NDAsImV4cCI6MjAxNDU5MjQ0MH0.dl06p8uSGa_qwdko6Fm-nzGeDasSxdJEexHpd1LPRI0",
+    options);
 
